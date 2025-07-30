45b11a7098c0a37d453b323d32706edacb322b85
AndreVictordmr <andre.victordmr@gmail.com>
2025-07-30 15:25:38 -0300

    Realizando atividade

--- js/02-meu-script.js
+++ js/02-meu-script.js
@@ -1,1 +1,10 @@
+/*Execicio
+  Crie uma variavel chamada "uc" e coloque o da UC que estamos
+  crie uma variavel chamda "competencia" e coloque nela uma frase que represente o que estamos estudanso nesta uc.
+  Monte e mostre uma frase no console exibindo a UC e a competêcia. Exemplo: Estamos na UC2 estudando Front-End com JavaScript. 
+*/
+
+let uc= 2,competencia='estudando as linguagem de Front-End, HTML, CSS e JS';
+
+console.log(`Estamos na UC${uc}, ${competencia} no curso desenvolvedor Full-Stack no Senac Penha `);
 
