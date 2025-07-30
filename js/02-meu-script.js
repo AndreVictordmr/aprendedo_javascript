61b6c4a4d8baf1a4122a1abea17590927ccd9e52
AndreVictordmr <andre.victordmr@gmail.com>
2025-07-30 16:39:16 -0300

    Aprendendo fazer citações
    aprendendo tambem  o uso das '\' no JS

--- js/02-meu-script.js
+++ js/02-meu-script.js
@@ -8,3 +8,12 @@ let uc= 2,competencia='estudando as linguagem de Front-End, HTML, CSS e JS';
 
 console.log(`Estamos na UC${uc}, ${competencia} no curso desenvolvedor Full-Stack no Senac Penha `);
 
+//Errado!
+//console.log("Gosto "muito" de paçoca!");
+//Correto! Aspas simples como comando, e duplas como texto
+console.log("Gosto 'muito' de paçoca!");
+//Correto! Aspas duplas como comando, e simples como texto
+console.log('Gosto "muito" de paçoca!');
+//Correto! Escapando as aspas como texto
+console.log('Gosto \'muito\' de paçoca!');
+
