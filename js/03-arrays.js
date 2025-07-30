645fd8e09d04357602abaffa841f8fe3ff4c4808
AndreVictordmr <andre.victordmr@gmail.com>
2025-07-30 17:27:02 -0300

    Aprendendo Array no JS

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,2 +1,12 @@
 'use strict';
 
+//declarando um array(vetor)
+let comidas = ['hamburguer','pudim','coxinha','esfira'];
+//Acessando um determinado elemento através do indice
+console.log(comidas[0]);
+// Se quisermos visualizar a estrutura do array inteiro(console):
+//console.log(comidas);
+console.log(`Eu talvez irei lanchar ${comidas[2]} é ${comidas[3]}`);
+
+
+
