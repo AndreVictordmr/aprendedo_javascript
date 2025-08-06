625185ef7997e80aff3b7802462d524dba952d17
AndreVictordmr <andre.victordmr@gmail.com>
2025-08-06 15:04:43 -0300

    Exercicio de vetor

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -13,7 +13,18 @@ console.log(`Eu talvez irei lanchar ${comidas[2]} é ${comidas[3]}`);
 let naosei=6;
 const coisas = [10,"Senac","<h2>Oie</h2>",naosei,15.88];
 console.log(coisas[1]);
-coisas.log(coisas[4])
+console.log(coisas[4])
+
+/*Exercicio 
+    1) Crie um array contendo o nome de 7 coisas que vc gosta (exemplos: artistas, músicas, livros, comida... sei lá, inventa!)
+
+    2) Em seguida, mostre no console uma frase personalizada indicando o segundo, o quinto e o sétimo elemento do array. Use concatenação e/ou template string.
+*/ 
+let mangas = ['One piece','Naruto','Eyeshild 21','Chainsaw Man','Spy Family','My Hero Academy','Dragon Ball'];
+let exibe =[1,4,6];
+exibe.forEach((i) => console.log(`Estes sao algum mangas que eu gosto ${mangas[i]}`)
+);
+
 
 
 
