693b2f7524971710bf2b57e35560ddf09a2ee7a3
AndreVictordmr <andre.victordmr@gmail.com>
2025-08-06 16:49:47 -0300

    Trabalhando com Objetos com Arrays

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -13,3 +13,22 @@ const pessoa = {
  // Acessamos as propriedades usando nomeDoObjeto.nomeDaPropriedade
 console.log(`O ${pessoa.nome_completo} tem ${pessoa.idade} anos é mora ${pessoa.cidade}-${pessoa.estado}`);
 
+//Exemplo 2: objeto com array
+console.log('\nExmplo2\n');
+
+const livro={
+    titulo: 'O Senhor dos Anéis',
+    autor: 'J.R.R Tolkien',
+    volumes: [
+        'A Sociedade do Anel',
+        'As Duas Torres',
+        'O Retorno do Rei'
+    ]
+};
+
+console.log(livro);
+console.log(livro.titulo);
+/*Para acessar algum dos volumes, primeros acessamos a propriedade(volumes) do objeto, entao usamos o colchetes com indice pois se trata de um array*/
+console.log(livro.volumes[1]);
+
+
