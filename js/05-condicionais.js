762ca0f67cd0822dd933f7db93c83bbf0f7e872b
AndreVictordmr <andre.victordmr@gmail.com>
2025-08-19 14:36:10 -0300

    Aprendendo condicionais

---
+++ js/05-condicionais.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*Comandos Condicionais mais comuns
+if -> Se
+else -> Senão
+*/
+
+console.log('Exemplo 1');
+
+let numero = 50;
+//Condicional simplie(usa apenas if)
+if(numero >= 10){
+    console.log('condiçao True');
+}
