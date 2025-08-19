21298e0f520ab0a060f66341698bba8c9152a2d7
AndreVictordmr <andre.victordmr@gmail.com>
2025-08-19 14:56:00 -0300

    Trabalhando com  if/else

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -12,3 +12,15 @@ let numero = 50;
 if(numero >= 10){
     console.log('condiçao True');
 }
+
+console.log('\nExemplo 2');
+
+let aluno = "Ozzy";
+let idade = 18;
+/* Logica: ferificar se o aluno e maior ou menor de idade */
+//Condicional Composta(if/else)
+if(idade >= 18){
+    console.log(`${aluno}, você maior de idade`);
+}else{
+    console.log(`${aluno}, você é menor de idade`);
+}
