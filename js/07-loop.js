e82b4572fdbe3600c7f32ea97b67b730eca2e972
AndreVictordmr <andre.victordmr@gmail.com>
2025-08-20 14:16:07 -0300

    Usando while

--- js/07-loop.js
+++ js/07-loop.js
@@ -1,1 +1,20 @@
 "use strict";
+
+/* Loops ou estruturas de repetiçao 
+    São comandos que, baseados em alguma condição lógica, são programados para executar ações repetidas vezes.
+    
+    Comandos mais tradicionais: while(Enquanto) e for (para)
+
+    Normalmente, os loops sáo controlados através de uma variável contadore, Esta variável é comumente chamado 'i','j','k' ou qualquer outra letra se necessário.
+
+*/
+
+console.log("Exemplo 1: WHILE(Enquanto)");
+
+let x = 1;
+
+while (x <= 5) {
+    console.log("Contagem regressiva: "+x);
+    x++;// = contador = contador +1;
+}
+
