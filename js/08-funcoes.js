87aba1bea7ee2714e65178675a5170e9f5eda2aa
AndreVictordmr <andre.victordmr@gmail.com>
2025-08-26 14:18:32 -0300

    O que e funçoes

--- js/08-funcoes.js
+++ js/08-funcoes.js
@@ -1,1 +1,12 @@
 'use strict';
+
+/*Sobre Funções
+Funções são bloco de código responsáveis por executar determinadas tarefas. Para usar estes blocos/funções, é necessário "chamar/invocar" a função desejada através do seu nome.
+
+Vantagens ao usar funções:
+- Reutilização de codigo
+- Organização de código-fonte, permitindo a separação de diferentes lógicas e responsabilidades
+- Performace melhor
+- Maior faciliadade para manutenções
+*/
+
