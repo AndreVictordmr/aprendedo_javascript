617a69d0bc7c7423714b9aa1ebeb8bdc3516f116
AndreVictordmr <andre.victordmr@gmail.com>
2025-08-27 14:01:42 -0300

    QuerySelectorAll

--- js/09-dom.js
+++ js/09-dom.js
@@ -3,10 +3,14 @@
 /*Exemplo 01 */
 
 // Procurando (querySelector) por um elemento chamado exemplo1
-const exemplo01=document.querySelector("#exemplo1");
+const exemplo01=document.getElementById("exemplo1");
 const mensagem01=document.querySelector("#mensagem01");
 const pagina=document.querySelector("body");
 
 console.log(exemplo01);
 console.log(mensagem01);
 console.log(pagina);
+//Para selecionar varios elementos, use querySelectorAll.
+//O resultado é uma especie de array de elementos (NodeList).
+const paragrafos=document.querySelectorAll("p");
+console.log(paragrafos);
