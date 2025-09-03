04077329094437cb39fbededfed3cef72de17e10
AndreVictordmr <andre.victordmr@gmail.com>
2025-09-03 17:21:48 -0300

    tirando boolean

--- js/10-senha.js
+++ js/10-senha.js
@@ -2,11 +2,10 @@
 
 const campoSenha = document.querySelector("#senha");
 const mostrar = document.querySelector("#visivel");
-let isVisivel;
-/*Monitoramos o evento pointerup/pointerdown pois ele funcionar tanto para o acionamento com mouse no desktop quanto com o dedo no moblie  */
+
+/*Monitoramos o evento pointerup/pointerdown pois ele funcionar tanto para o acionamento com mouse no desktop quanto com o dedo no moblie */ 
 mostrar.addEventListener("pointerdown",function(){
     campoSenha.type='text';
-    isVisivel = true;
     
 });
 mostrar.addEventListener("pointerup",function(){
