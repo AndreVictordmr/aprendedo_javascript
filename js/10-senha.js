056fae33ecf7cc63a3203b34e134f2da5d3eaa50
AndreVictordmr <andre.victordmr@gmail.com>
2025-09-03 16:31:28 -0300

    Deixando visível

--- js/10-senha.js
+++ js/10-senha.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const campoSenha = document.querySelector("#senha");
+const mostrar = document.querySelector("#visivel");
+let isVisivel;
+/*Monitoramos o evento pointerup/pointerdown pois ele funcionar tanto para o acionamento com mouse no desktop quanto com o dedo no moblie  */
+mostrar.addEventListener("pointerdown",function(){
+    campoSenha.type='text';
+    isVisivel = true;
+    
+});
+mostrar.addEventListener("pointerup",function(){
+    campoSenha.type="password";
+});
+mostrar.addEventListener("pointerout",function(){
+    campoSenha.type="password";
+});
